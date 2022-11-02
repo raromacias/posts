4b18421b207236bcccd534b5751163ad3cefeb61
Raro Macias <ggmacias@gmail.com>
2022-11-02 17:27:31 -0400

    added functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,12 @@
+require('dotenv').config()
+const {SECRET} = process.env
+const {User} = require('../models/user')
+const bcrypt = require(`bcryptjs`)
+const jwt = require('jsonwebtoken')
+
+const createToken = (username, id) => {
+          return jwt.sign({username, id}, SECRET, {expiresIn: '2 days'})
+}
 
 module.exports = {
 
@@ -12,8 +21,25 @@ logout: (req, res) => {
     console.log('logged out')
 },
 
-register: (req, res) => {
-    console.log('registered')
+register: async (req, res) => {
+    try {
+       const {username, password} = req.body
+       const foundUser = await User.findOne({where: {username:username}})
+       if(foundUser){
+        res.status(400).send("User already exists")
+       } else {
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(password, salt)
+        const newUser = await User.create({username:username, hashedPass: hash})
+        const token = createToken(newUser.dataValues.username,newUser.dataValues.id)
+        console.log(newUser)
+        const exp = Date.now() + 1000 * 60 * 60 * 48
+        res.status(200).send({username:newUser.dataValues.username, userId:newUser.dataValues.id, token: token, exp:exp})
+       }
+    } catch (error) {
+        console.log(error)
+        res.status(400).send(error)
+    }
 }
 }
 
